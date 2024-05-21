9251460936fc3310ba6ef17c157ab2b295717a70
dafyn <102889770+dafyn@users.noreply.github.com>
2024-05-21 18:39:41 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,41 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-//import { Fund } from 'react-bootstrap-icons';
-import logo from '../Assets/olum-nobg.png';
+import Container from 'react-bootstrap/Container';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { GlobalOutlined } from '@ant-design/icons';
 
 const HeaderComponent = () => {
-  return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-      <Link className="navbar-brand" to="/">
-        <img src={logo} alt="Logo" style={{ width: '150px', height: 'auto' }} />
-      </Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/projects">Projects</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">About Us</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/news">News</Link>
-          </li>
-        </ul>
-        <Button variant="primary">
-          {/* <Fund size={20} /> */}
-           Donate
-        </Button>
+    const headerStyle = {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100%',
+      backgroundColor: 'white',
+      zIndex: 1000 // Ensure the header appears above other content
+    };
+  
+    return (
+      <div style={headerStyle}>
+        <Container>
+          <div className="d-flex justify-content-between align-items-center py-2">
+            <div>Be the change you want in the world</div>
+            <Dropdown>
+              <Dropdown.Toggle variant="light" id="dropdown-basic">
+                <GlobalOutlined />
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item>English</Dropdown.Item>
+                <Dropdown.Item>Swahili</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        </Container>
       </div>
-    </nav>
-  );
-}
+    );
+  }
 
 export default HeaderComponent;
