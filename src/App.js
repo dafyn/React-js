c92ba5625c6633d794b78f1fb426391e43200aa5
dafyn <102889770+dafyn@users.noreply.github.com>
2024-05-21 18:45:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import { Layout } from 'antd';
-import About from './Pages/AboutUs';
-import ProjectDetail from './Pages/ProjectDetails';
-import Projects from './Pages/Projects';
-import News from './Pages/News';
-import Home from './Pages/Home';
+import HomeLayout from './Layouts/HomeLayout';
+import AboutLayout from './Layouts/AboutUsLayout';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import FooterComponent from './Components/Footer';
-import NavbarComponent from './Components/NavBar';
 // import 'antd/dist/antd.css';
 import './App.css';
-
-const { Content } = Layout;
 
 const App = () => {
   return (
     <Router>
-      <Layout>
-        <NavbarComponent />
-        <Content style={{ padding: '0 50px', marginTop: 64 }}>
-          <div className="site-layout-content">
+          {/* <div className="site-layout-content"> */}
             <Routes> 
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/projects" component={Projects} />
-              <Route path="/project/:id" component={ProjectDetail} />
-              <Route path="/news" component={News} />
+              <Route path='/' element={<HomeLayout />} />
+              <Route path='/about' element={<AboutLayout />} />
             </Routes>  
-          </div>
-        </Content>
-        <FooterComponent />
-      </Layout>
+          {/* </div> */}
     </Router>
   );
 }
